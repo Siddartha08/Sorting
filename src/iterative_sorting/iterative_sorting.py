a7f3c618babe99946850e150fa2c6ba1664c7ec8
unknown <jemery89@gmail.com>
2019-06-10 22:08:44 -0500

    jp emery recursion completed, problems with bubble

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,20 +6,33 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
 
-
-
-        # TO-DO: swap
-
-
-
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+        
+      
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    # position = 0
+    # length = len(arr)
+
+    # for i in range(len(arr) -1,0,1 ):
+    #     for j in range(i):
+    #         tempVal = arr[j]
+    #         arr[j] = arr[j + 1]
+    #         arr[j + 1] = tempVal
+    # missing a loop around this. 
+    length = len(arr)
+    for i in range(arr):
+        if arr[i] > arr[i + 1]:
+            tempVal = arr[i + 1]
+            arr[i + 1] = arr[i]
+            arr[i] = tempVal
 
     return arr
 
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,31 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    # merged_arr = [0] * elements
+    merged_arr = arrA + arrB
     # TO-DO
-    
+    # merged_arr = arrA.join(arrB)
+    # merged_arr = arrA + arrB
+    print(merged_arr)
+    merged_arr.sort()
+    print(merged_arr)
     return merged_arr
 
-
+arr = [1,2,3,4,5]
+arr2 = [9,8,7,6]
+merge(arr, arr2)
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) == 0 or len(arr) ==1:
+        return arr
+    else:
+        length = len(arr)
+        half = int(length)/2
+
+        firstHalf = merge_sort(arr[:int(half)])
+        secondHalf = merge_sort(arr[int(half):])
+        return merge(firstHalf, secondHalf)
 
     return arr
 
@@ -31,3 +47,12 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+# def foo(n):
+#     if n == 0:
+#         return
+
+#     print(n)
+
+#     foo(n - 1)
